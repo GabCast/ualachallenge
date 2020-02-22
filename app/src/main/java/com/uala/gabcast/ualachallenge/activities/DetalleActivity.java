8a721dbcc7aa83c6b765b562b686c2bf9b386b13
GabiCast <gcastaneda@gmssa.com.ar>
2020-02-22 11:52:33 -0300

    ejercicio 6

--- app/src/main/java/com/uala/gabcast/ualachallenge/activities/DetalleActivity.java
+++ app/src/main/java/com/uala/gabcast/ualachallenge/activities/DetalleActivity.java
@@ -1,12 +1,18 @@
 package com.uala.gabcast.ualachallenge.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.google.gson.Gson;
 import com.uala.gabcast.ualachallenge.R;
 import com.uala.gabcast.ualachallenge.adapters.MealsRecyclerAdapter;
@@ -15,16 +21,18 @@ import com.uala.gabcast.ualachallenge.entitites.Meal;
 
 import java.util.ArrayList;
 
-public class DetalleActivity extends AppCompatActivity {
+public class DetalleActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
 
+    private Meal meal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle);
 
-        Meal meal = getMealIntent();
-        setResources(meal);
-        setRecyclerView(meal);
+        meal = getMealIntent();
+        setResources();
+        setRecyclerView();
+
     }
 
     private Meal getMealIntent(){
@@ -32,15 +40,17 @@ public class DetalleActivity extends AppCompatActivity {
         return new Gson().fromJson(a, Meal.class);
     }
 
-    private void setResources(Meal meal){
+    private void setResources(){
         TextView txtNombre = (TextView) findViewById(R.id.txtNombre);
         txtNombre.setText(meal.getStrMeal());
         TextView txtInstrucciones = (TextView) findViewById(R.id.txtInstrucciones);
         txtInstrucciones.setText(meal.getStrInstructions());
+        YouTubePlayerView youtube = (YouTubePlayerView) findViewById(R.id.youtube);
+        youtube.initialize(getResources().getString(R.string.Youtube), this);
     }
 
 
-    public void setRecyclerView(Meal meal) {
+    public void setRecyclerView() {
         RecyclerView rcvIngredientes = (RecyclerView) findViewById(R.id.rcvIngredientes) ;
         GridLayoutManager lLayout = new GridLayoutManager(this, 2);
         rcvIngredientes.setHasFixedSize(true);
@@ -49,4 +59,16 @@ public class DetalleActivity extends AppCompatActivity {
         rcvIngredientes.setAdapter(rcAdapter);
     }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if (!b) {
+            String[] url = meal.getStrYoutube().split("=");
+            youTubePlayer.cueVideo(url[1]);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
 }
